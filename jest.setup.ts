22f4bd193d774d34548ebb95fd9143a3863cb0f2
Connor Willis <connorwillis05@gmail.com>
2026-04-18 22:34:58 -0700

    fixing tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,10 @@
 import { fetch, Request, Response, Headers } from 'undici';
 
 Object.assign(global, { fetch, Request, Response, Headers });
+
+// Set a dummy TMDB_API_KEY for tests
+process.env.TMDB_API_KEY = 'test-key';
+
+// Prevent accidental real API calls
+jest.spyOn(global, 'fetch').mockRejectedValue(new Error('Unexpected fetch call - use mockFetchSequence in your test'));
+
